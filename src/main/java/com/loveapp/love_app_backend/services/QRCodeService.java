dba77801e798481258f2740668e50ff075faf5af
felipegresele <felipehgresele@gmail.com>
2026-04-23 17:42:49 -0300

    fix: ajuste posicao qr code frame JUNTOS

--- src/main/java/com/loveapp/love_app_backend/services/QRCodeService.java
+++ src/main/java/com/loveapp/love_app_backend/services/QRCodeService.java
@@ -31,7 +31,7 @@ public class QRCodeService {
     // SURPRESA: QR 300x300 centralizado na área branca (área maior comporta)
     private static final Map<String, int[]> FRAME_QR_BOUNDS = Map.of(
             "ESCANEIE", new int[]{149, 157, 200, 200},
-            "JUNTOS",   new int[]{180, 150, 200, 200},
+            "JUNTOS",   new int[]{270, 280, 200, 200},
             "SPOTIFY",  new int[]{150, 150, 200, 200},
             "SURPRESA", new int[]{ 99, 103, 300, 300},
             "CARTA", new int[]{ 140, 150, 200, 200}
